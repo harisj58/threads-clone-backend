43228c6b35607a8e75aac9243f22156c19aa1a3c
Haris Javed <harisj58@gmail.com>
2023-10-29 12:07:07 +0530

    enabled cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
 
 // dotenv allows us to use the process.env variables in our app
 dotenv.config();
@@ -12,6 +13,29 @@ dotenv.config();
 const app = express();
 // connect to the database
 connectDB();
+
+// Cross-Origin Resource Sharing (CORS) is an HTTP-header based
+// mechanism that allows a server to indicate any origins (domain, scheme,
+// or port) other than its own from which a browser should permit loading
+// of resources. CORS also relies on a mechanism by which browsers make a
+// "preflight" request to the server hosting the cross-origin resource,
+// in order to check that the server will permit the actual request.
+// In that preflight, the browser sends headers that indicate the HTTP
+// method and headers that will be used in the actual request.
+app.use(cors());
+app.options("*", cors({ origin: true, credentials: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // set the PORT to the env variable or default to 5000
 const PORT = process.env.PORT || 5000;
